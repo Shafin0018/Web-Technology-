779849ff986ffbe9a34f1dd8cd7d97bcafa427cf
Shafin0018 <ahmefsafin64@gmail.com>
2025-07-20 11:20:20 +0600

    Add files via upload

---
+++ lab task 1 md shafin.php
@@ -0,0 +1,79 @@
+ <!DOCTYPE html>
+<html>
+<body>
+    <center>
+        <h1 style="color:blue">AIUB</h1>
+         <h3 style="color:blue">Registration Form</h3>
+</center>
+<h3 > Complete the Registration</h3>
+<table>
+    <tr>
+        <td> Full name:</td>
+        <td><input type="text"></td>
+</tr>
+ <tr>
+        <td> Email:</td>
+        <td><input type="email"></td>
+</tr>
+ <tr>
+        <td> Password:</td>
+        <td><input type="password"></td>
+</tr>
+
+ <tr>
+        <td> Gender</td>
+        <td>
+           <input type ="radio"name="des">male
+
+</td>
+
+ <td>
+           <input type ="radio"name="des">female
+</td>
+</tr>
+ <tr>
+        <td>language known</td>
+        <td>
+           <input type ="radio"name="des">English
+</td>
+ <td>
+           <input type ="radio"name="des">Bangla
+</td>
+ <td>
+           <input type ="radio"name="des">Hindi
+</td>
+</tr>
+<tr>
+    <td> Country</td>
+        <td>
+            <select>
+            <option name="Select">--Select--</option>
+           <option name="Bangladesh">Bangladesh</option>
+            <option name="India">India</option>
+             <option name="UK">UK</option>
+</select>
+<tr>
+    <td>Date Of Birth:</td>
+    <td><input type="date"></td>
+
+</tr>
+<tr>
+    <td>Photo Upload:</td>
+    <td><input type="file"></td>
+
+</tr>
+<tr>
+        <td>Comments:</td>
+    <td><input type="text"></td>
+</tr>
+</table>
+<tr>
+<td> </td>
+<td> <input type ="button" value ="Register"></td>
+</tr>
+</body>
+</html>
+
+
+
+        
